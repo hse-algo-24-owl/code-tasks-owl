cd346223846e69094d952b6fdd5b32b249064727
oppewalas <oppewala@yandex.ru>
2024-11-20 20:52:50 +0500

    Задание выполнено (Иванов Артем, РИС-23-4)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from profilehooks import profile
-
+"""from profilehooks import profile"""
 
 def fibonacci_rec(n: int) -> int:
     """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
@@ -8,6 +7,10 @@ def fibonacci_rec(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    if n < 3:
+        return 1
+    else:
+        return fibonacci_rec(n-1)+fibonacci_rec(n-2)
     pass
 
 
@@ -18,6 +21,13 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    fib = [0] * n
+    fib[0] = 1
+    if n > 1:
+        fib[1] = 1
+    for i in range(2, n):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib[n - 1]
     pass
 
 
@@ -27,6 +37,10 @@ def fibonacci(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    a, b = 1, 1
+    for i in range (2, n):
+        a, b = b, a+b
+    return b
     pass
 
 
@@ -39,7 +53,7 @@ def main():
     print(fibonacci_iter(n))
 
     print(f"\nВычисление {n} числа Фибоначчи итеративно без использования массива:")
-    print(fibonacci_iter(n))
+    print(fibonacci(n))
 
 
 if __name__ == "__main__":
