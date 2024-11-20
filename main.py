26096d7dcd75b1ad7dd2b341257103b3f6aabbf8
Ahman15kyptok <maximhiro@gmail.com>
2024-11-20 22:30:36 +0300

    task-1

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
-from profilehooks import profile
+def fibonacci_rec(n): # Рекурсивный метод решения
+    if n <= 1:
+        return n
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
+
+def fibonacci_iter(n): # Итерационный с использованием массива
+    fib = [0] * (n + 1)
+    fib[0] = 0
+    fib[1] = 1
+
+    for i in range(2, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib[n]
 
 
-def fibonacci_rec(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
-    формуле вычисления последовательности.
+def fibonacci(n): # Итерационный без использования массива.
+    if n <= 1:
+        return n
 
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
 
-
-def fibonacci_iter(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
-    массива для хранения вычисляемых данных.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
-
-
-def fibonacci(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована итеративно без использования массива.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
-
+    return b
 
 def main():
-    n = 35
+    n = 36
     print(f"Вычисление {n} числа Фибоначчи рекурсивно:")
     print(fibonacci_rec(n))
 
