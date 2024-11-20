6d7628ec4a6d69e61c30795b240ec500ba33e283
AlinaRN4 <ar.nurtdinova4@gmail.com>
2024-11-20 22:25:13 +0500

    Команда Ootd выполнила задание

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ from profilehooks import profile
 def fibonacci_rec(n: int) -> int:
     """Возвращает N-е число Фибоначчи. Реализована рекурсивно согласно
     формуле вычисления последовательности.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
     """
+    if (n == 1) or (n == 2):
+        return 1
+    else:
+      return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
     pass
 
 
 def fibonacci_iter(n: int) -> int:
     """Возвращает N-е число Фибоначчи. Реализована итеративно с использованием
     массива для хранения вычисляемых данных.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
     """
+    if (n <= 0):
+        return 0
+    elif (n == 1 or n == 2):
+        return 1
+
+    fib = [0] * n
+    fib[0] = 1
+    fib[1] = 1
+
+    for i in range(2, n):
+        fib[i] = fib[i - 1] + fib[i - 2]
+
+    return fib[n - 1]
     pass
 
 
@@ -27,6 +38,19 @@ def fibonacci(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
+    if (n <= 0):
+        return 0
+    elif (n == 1 or n == 2):
+        return 1
+    fibMinus2 = 1
+    fibMinus1 = 1
+    fibN = 0
+    
+    for i in range (2, n):
+        fibN = fibMinus2 + fibMinus1
+        fibMinus2 = fibMinus1
+        fibMinus1 = fibN
+    return fibN
     pass
 
 
