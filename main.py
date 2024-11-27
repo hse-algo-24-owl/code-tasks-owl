ec9755c49474e80dd1e86eb323f2f71f54227b4c
alenaav <alena.andrienko.2018@bk.ru>
2024-11-27 20:48:48 +0500

    Команда ootd выполнила задание 2

--- main.py
+++ main.py
@@ -6,7 +6,33 @@ def calculate_determinant(matrix: list[list[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    validate(matrix)
+    order = len(matrix)
+    if order == 1:
+        return matrix[0][0]
+    elif order == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for idx, value in enumerate(matrix[0]):
+        if value == 0:
+            continue
+        minor = [row[:idx] + row[idx + 1 :] for row in matrix[1:]]
+        co_factor = (-1) ** idx
+        det += value * co_factor * calculate_determinant(minor)
+
+    return det
+
+
+def validate(matrix: list[list[int]]) -> None:
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise Exception("Матрица пуста или не является списком")
+    for row in matrix:
+        if not isinstance(row, list) or len(row) != len(matrix):
+            raise Exception("Матрица не является квадратной")
+        for item in row:
+            if not isinstance(item, int):
+                raise Exception("Не все элементы матрицы являются целыми числами")
 
 
 def main():
