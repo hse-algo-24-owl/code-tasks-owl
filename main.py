1330791123add077dce92a5e28105748d472f7b6
BillyTheVkid <your.email@example.com>
2024-11-28 18:40:08 +0500

    task 2

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+def validate(matrix: list[list[int]]):
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise Exception("Матрица пустая")
+    for row in matrix:
+        if not isinstance(row, list) or len(row) ==0:
+            raise Exception("Строка матрицы пустая")
+        elif len(matrix) != len(row):
+            raise Exception("Матрица не квадратная")
+    for item in row:
+        if not isinstance(item, int):
+            raise Exception("Матрица содержит не целочисленные значения")
+         
 def calculate_determinant(matrix: list[list[int]]) -> int:
-    """Вычисляет определитель целочисленной квадратной матрицы
+    validate(matrix)        
+    if len(matrix) == 1:
+        return matrix[0][0]
 
-    :param matrix: целочисленная квадратная матрица
-    :raise Exception: если значение параметра не является целочисленной
-    квадратной матрицей
-    :return: значение определителя
-    """
-    pass
-
+    det = 0
+    for col_idx, value in enumerate(matrix[0]):
+        if value == 0:
+            continue
+        reduced_matrix = [[item for item in row] for str_idx, row in enumerate(matrix) if str_idx != 0]
+        reduced_matrix = [[item for j, item in enumerate(row) if j != col_idx] for row in reduced_matrix]
+        co_factor =  (-1)**col_idx * calculate_determinant(reduced_matrix)
+        det += value*co_factor
+    return det
 
 def main():
     matrix = [[1, 2], [3, 4]]
@@ -20,3 +36,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
