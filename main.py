644961a838086610d59b3369b9da3d6232e0981d
aksma <aksmaxn4@gmail.com>
2024-12-02 17:32:03 +0500

    solved

--- main.py
+++ main.py
@@ -20,7 +20,31 @@ def generate_strings(length: int) -> list[str]:
     числом.
     :return: Список строк.
     """
-    pass
+    if (not isinstance(length, int) or length <= 0):
+        raise ValueError(STR_LENGTH_ERROR_MSG)
+
+    strings = []
+    curr_string = ""
+    add_one_to_str(curr_string, strings, length)
+    add_zero_to_str(curr_string, strings, length)
+    return strings
+
+
+def add_one_to_str(curr_str, strings, length):
+    if len(curr_str) == length:
+        strings.append(curr_str)
+        return
+
+    add_one_to_str(curr_str + '1', strings, length)
+    add_zero_to_str(curr_str + '1', strings, length)
+
+
+def add_zero_to_str(curr_str, strings, length):
+    if len(curr_str) == length:
+        strings.append(curr_str)
+        return
+    
+    add_one_to_str(curr_str + '0', strings, length)
 
 
 def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
@@ -32,7 +56,29 @@ def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
     числами или значение параметра n меньше чем k.
     :return: Значение биномиального коэффициента.
     """
-    pass
+    
+    if not isinstance(n, int):
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("n", n))
+    if not isinstance(k, int):
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("k", k))
+    if n < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("n", n))
+    if k < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("k", k))
+    if n < k:
+        raise ValueError(N_LESS_THAN_K_ERROR_MSG.format(n, k))
+    
+    if use_rec:
+        if k == 0:
+            return 1
+        if k == n:
+            return 1
+        return binomial_coefficient(n - 1, k, use_rec=True) + binomial_coefficient(n - 1, k - 1, use_rec=True)
+    else:
+        result = 1
+        for i in range(0, k):
+            result = result * (n - i) // (i + 1)
+        return result
 
 
 def main():
