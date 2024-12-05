d83362dd190a6bc679451305d5f7bdefaf74e100
GergertLA <ludmila-gergert-13@yandex.com>
2024-12-05 20:31:37 +0500

    main.py

--- main.py
+++ main.py
@@ -20,7 +20,40 @@ def generate_strings(length: int) -> list[str]:
     числом.
     :return: Список строк.
     """
-    pass
+    if not type(length) == int:
+        raise ValueError(STR_LENGTH_ERROR_MSG)
+    if length < 1:
+        raise ValueError(STR_LENGTH_ERROR_MSG)
+    if not isinstance(length, int) or length <= 0:
+        raise ValueError(STR_LENGTH_ERROR_MSG)
+    if length == 1:
+        return ['1', '0']
+
+    strings = []
+    curr_string = ""
+    add_one_to_str(curr_string, strings, length)
+    add_zero_to_str(curr_string, strings, length)
+    return strings
+
+
+def add_one_to_str(curr_str, strings, length):
+    if len(curr_str) == length:
+        if curr_str not in strings:
+            strings.append(curr_str)
+        return
+
+    add_one_to_str(curr_str + '1', strings, length)
+    add_zero_to_str(curr_str + '1', strings, length)
+
+
+def add_zero_to_str(curr_str, strings, length):
+    if len(curr_str) == length:
+        if curr_str not in strings:
+            strings.append(curr_str)
+        return
+
+    if not curr_str or curr_str[-1] != '0':
+        add_one_to_str(curr_str + '0', strings, length)
 
 
 def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
@@ -32,11 +65,33 @@ def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
     числами или значение параметра n меньше чем k.
     :return: Значение биномиального коэффициента.
     """
-    pass
+    
+    if not isinstance(n, int):
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("n", n))
+    if not isinstance(k, int):
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("k", k))
+    if n < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("n", n))
+    if k < 0:
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("k", k))
+    if n < k:
+        raise ValueError(N_LESS_THAN_K_ERROR_MSG.format(n, k))
+    
+    if use_rec:
+        if k == 0:
+            return 1
+        if k == n:
+            return 1
+        return binomial_coefficient(n - 1, k, use_rec=True) + binomial_coefficient(n - 1, k - 1, use_rec=True)
+    else:
+        result = 1
+        for i in range(0, k):
+            result = result * (n - i) // (i + 1)
+        return result
 
 
 def main():
-    n = 10
+    n = 5
     print(f"Строки длиной {n}:\n{generate_strings(n)}")
 
     n = 30
