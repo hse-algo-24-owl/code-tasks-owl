246def88c134ea81f350662561f10893a89b74c7
Захар Солдаткин <zaharsoldatkin@MacBook-Air-Zahar.local>
2024-12-12 19:26:40 +0500

    task-4-plus-v-2 completed

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class ProfitValueError(Exception):
 def get_invest_distributions(
     profit_matrix: list[list[int]],
 ) -> dict[str:int, str : list[list[int]]]:
-    """Рассчитывает максимально возможную прибыль и распределение инвестиций
+    """
+    Рассчитывает максимально возможную прибыль и распределение инвестиций
     между несколькими проектами. Инвестиции распределяются кратными частями.
     :param profit_matrix: Таблица с распределением прибыли от проектов в
     зависимости от уровня инвестиций. Проекты указаны в столбцах, уровни
@@ -33,11 +34,62 @@ def get_invest_distributions(
     distributions - списком со всеми вариантами распределения инвестиций между
     проектами, обеспечивающими максимальную прибыль.
     """
-    pass
+
+    # Проверка на пустой список
+    if profit_matrix is None or not profit_matrix or not profit_matrix[0]:
+        raise ValueError(PARAM_ERR_MSG)
+
+    # Проверка кадратности матрицы и её значений
+    for row_idx, row in enumerate(profit_matrix):
+        if len(row) != len(profit_matrix[0]):
+            raise ValueError(PARAM_ERR_MSG)
+        
+        for project_idx, value in enumerate(row):
+            if not isinstance(value, int):
+                raise ValueError(PARAM_ERR_MSG)
+            if value < 0:
+                raise ProfitValueError(NEG_PROFIT_ERR_MSG, project_idx, row_idx)
+
+    # Проверка на возрастание прибыли с ростом инвестиций
+    for project_idx in range(len(profit_matrix[0])):
+        for row_idx in range(1, len(profit_matrix)):
+            if profit_matrix[row_idx][project_idx] < profit_matrix[row_idx - 1][project_idx]:
+                raise ProfitValueError(DECR_PROFIT_ERR_MSG, project_idx, row_idx)
+
+    dp = [[0] * (len(profit_matrix[0]) + 1) for _ in range(len(profit_matrix) + 1)]  # Массив для рассчета прибыли
+    dp_options = [[[[]] for _ in range(len(profit_matrix[0]) + 1)] for _ in range(len(profit_matrix) + 1)] # Массив путей
+
+
+    for i in range(len(profit_matrix) + 1):
+        for j in range(1, len(profit_matrix[0]) + 1):
+            for k in range(i + 1):
+                # Рассчёт прибыли
+                if k == 0:
+                    profit = dp[i - k][j - 1]
+                else:
+                    profit = dp[i - k][j - 1] + profit_matrix[k - 1][j - 1]
+
+                # Обработка путей
+                if profit > dp[i][j]:
+                    dp[i][j] = profit
+                    dp_options[i][j] = [g + [k] for g in dp_options[i - k][j - 1]]
+                elif profit == dp[i][j]:
+                    dp_options[i][j] += [g + [k] for g in dp_options[i - k][j - 1]]
+
+    return {PROFIT: dp[len(profit_matrix)][len(profit_matrix[0])], DISTRIBUTIONS: [g for g in dp_options[len(profit_matrix)][len(profit_matrix[0])] if len(g) == len(profit_matrix[0])]}
 
 
 def main():
-    profit_matrix = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+#     profit_matrix = [
+#     [11, 13, 14, 12],
+#     [15, 17, 19, 21],
+#     [22, 25, 28, 31],
+#     [36, 32, 40, 44],
+#     [51, 54, 53, 52],
+# ]
+    profit_matrix = [[1, 1, 1],
+                     [2, 2, 2],
+                     [3, 3, 3]]
     print(get_invest_distributions(profit_matrix))
 
 
